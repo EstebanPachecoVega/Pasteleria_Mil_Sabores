c1949cf800ad3f3da54e8084ae94def7b417a28b
EstebanPachecoVega <es.pachecov@duocuc.cl>
2025-09-11 19:18:42 -0300

    Carga de Perfil
    Se agrega config.js para que funcione correctamente las rutas

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+// Configuracion de rutas para los archivos parciales
+export const PATHS = {
+  PARTIALS: {
+    HEADER: '/src/partials/header.html',
+    NAVBAR: '/src/partials/navbar.html', 
+    FOOTER: '/src/partials/footer.html'
+  }
+};
